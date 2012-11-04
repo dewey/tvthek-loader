0b4d21a5d765527e98729cd501eff380bdef10a6
dewey <mail@notmyhostna.me>
2012-11-04 19:00:02 +0100

    Added Chapter selection

--- tvthek-loader.py
+++ tvthek-loader.py
@@ -5,10 +5,11 @@ import os
 from BeautifulSoup import BeautifulSoup
 
 def rtmpString (stream, show):
-	return ('rtmpdump -r "rtmp://apasfw.apa.at/cms-worldwide/" -a "cms-worldwide/" -f "WIN 11,1,102,55" -W "http://tvthek.orf.at/flash/player/TVThekPlayer_9_ver18_1.swf" -p "'+ show +'" -y "'+ stream +'" -v -o ' + output)
+	return ('rtmpdump -r "rtmp://apasfw.apa.at/cms-worldwide/" -a "cms-worldwide/" -f "WIN 11,1,102,55" -W "http://tvthek.orf.at/flash/player/TVThekPlayer_9_ver18_1.swf" -p "'+ show +'" -y "'+ stream +'" -v -o ' + output + ".flv")
 
 showlink = sys.argv[1]
 output = sys.argv[2]
+chapter = int(sys.argv[3])-1
 
 r = requests.get(showlink)
 
@@ -22,7 +23,7 @@ playerCodeString = playerCode.contents
 for item in playerCodeString:
 	m = re.search('mp4\:([^:]*?)mp4',item)
 
-streamlink = m.group(0)
+streamlink = m.group(chapter)
 
 os.system(rtmpString(streamlink,showlink))
 
